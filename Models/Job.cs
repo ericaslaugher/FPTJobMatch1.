d82b1ffa74bef628458ae96c488f0a4f2264cd5d
cakho <cakho@DESKTOP-95POVAI>
2024-10-09 13:45:41 +0700

    Add project files.

---
+++ Models/Job.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
+
+namespace FPTJobMatch.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Range(250, 50000)]
+        public int Salary {  get; set; }
+        [Required]
+        public string Place {  get; set; }
+        [Required]
+        public string Time {  get; set; }
+    }
+}
